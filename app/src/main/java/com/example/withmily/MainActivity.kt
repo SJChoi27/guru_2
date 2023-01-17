10d77aeefde01f3f41859c3926ce38926a9b66ff
Seojeong Choi <Seojeong5674@gmail.com>
2023-01-18 00:36:49 +0900

    달력 구현 안드로이드 기본 달력 활용, 나중에 원하는 스타일로 변형시켜야 함

--- app/src/main/java/com/example/withmily/MainActivity.kt
+++ app/src/main/java/com/example/withmily/MainActivity.kt
@@ -1,11 +1,188 @@
 package com.example.withmily
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+import android.view.View
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    //xml에 생성해둔 ui 정보를 호출해서 변수로 생성
+    var userID: String = "userID"
+    lateinit var fname: String
+    lateinit var str: String
+    lateinit var calendarView: CalendarView
+    lateinit var updateBtn: Button
+    lateinit var deleteBtn:Button
+    lateinit var saveBtn:Button
+    lateinit var diaryTextView: TextView
+    lateinit var diaryContent:TextView
+    lateinit var title:TextView
+    lateinit var contextEditText: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // UI값 생성
+        calendarView=findViewById(R.id.calendarView)
+        diaryTextView=findViewById(R.id.diaryTextView)
+        saveBtn=findViewById(R.id.saveBtn)
+        deleteBtn=findViewById(R.id.deleteBtn)
+        updateBtn=findViewById(R.id.updateBtn)
+        diaryContent=findViewById(R.id.diaryContent)
+        title=findViewById(R.id.title)
+        contextEditText=findViewById(R.id.contextEditText)
+
+        title.text = "달력 일기장"
+
+        //달력 클릭 이벤트
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            diaryTextView.visibility = View.VISIBLE //클릭 했을 시에 하단의 날짜 활성화
+            saveBtn.visibility = View.VISIBLE //저장 버튼 활성화
+            contextEditText.visibility = View.VISIBLE //내용 입력창 활성화
+            diaryContent.visibility = View.INVISIBLE //
+            updateBtn.visibility = View.INVISIBLE //수정 버튼 비활성화
+            deleteBtn.visibility = View.INVISIBLE //삭제 버튼 비활성화
+            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth) //하단의 날짜 숫자 표시
+            contextEditText.setText("") //내용입력창 클릭시 타자 활성화
+            checkDay(year, month, dayOfMonth, userID) //checkDay 함수에 변수 넣어서 활용
+        }
+
+        //저장 버튼 클릭 이벤트
+        saveBtn.setOnClickListener {
+            saveDiary(fname) //입력받은 문장 저장
+            contextEditText.visibility = View.INVISIBLE //내용 입력창 비활성화
+            saveBtn.visibility = View.INVISIBLE //저장 버튼 비활성화
+            updateBtn.visibility = View.VISIBLE //수정 버튼 활성화
+            deleteBtn.visibility = View.VISIBLE //삭제 버튼 활성화
+            str = contextEditText.text.toString() //입력받은 문장을 변수에 저장
+            diaryContent.text = str //문장 저장해놓은 변수를 보여주는 창
+            diaryContent.visibility = View.VISIBLE //활성화
+        }
+    }
+
+    // 달력 내용 조회, 수정
+    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userID: String) {
+        //저장할 파일 이름설정
+        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
+
+        var fileInputStream: FileInputStream
+        try {
+            fileInputStream = openFileInput(fname)
+            val fileData = ByteArray(fileInputStream.available())
+            fileInputStream.read(fileData)
+            fileInputStream.close()
+            str = String(fileData)
+            contextEditText.visibility = View.INVISIBLE
+            diaryContent.visibility = View.VISIBLE
+            diaryContent.text = str
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            updateBtn.setOnClickListener {
+                contextEditText.visibility = View.VISIBLE
+                diaryContent.visibility = View.INVISIBLE
+                contextEditText.setText(str)
+                saveBtn.visibility = View.VISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryContent.text = contextEditText.text
+            }
+            deleteBtn.setOnClickListener {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                contextEditText.setText("")
+                contextEditText.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                removeDiary(fname)
+            }
+            if (diaryContent.text == null) {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryTextView.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                contextEditText.visibility = View.VISIBLE
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    // 달력 내용 제거
+    @SuppressLint("WrongConstant")
+    fun removeDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = ""
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    // 달력 내용 추가
+    @SuppressLint("WrongConstant")
+    fun saveDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = contextEditText.text.toString()
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
