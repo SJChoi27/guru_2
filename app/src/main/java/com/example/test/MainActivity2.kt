9521f82e63109be6bc7fef7ff3bbc3c5456f12e5
Seojeong Choi <Seojeong5674@gmail.com>
2023-01-23 18:44:47 +0900

    달력과 게시글 연결까지 완료 (미완 - 사진 첨부와 디자인 수정)

--- app/src/main/java/com/example/test/MainActivity2.kt
+++ app/src/main/java/com/example/test/MainActivity2.kt
@@ -1,13 +1,27 @@
 package com.example.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
 
+    lateinit var fixButton : Button
+    lateinit var diaryTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        fixButton = findViewById(R.id.fixButton)
+
+        fixButton.setOnClickListener {
+            var intent = Intent(this, addCalendarActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
