44d4ac164d7bede26052dd69ef5e2317766ce24a
Seojeong Choi <Seojeong5674@gmail.com>
2023-01-25 22:08:09 +0900

    시작화면 구성중

--- app/src/main/java/com/example/test/FirstActivity.kt
+++ app/src/main/java/com/example/test/FirstActivity.kt
@@ -5,7 +5,11 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Switch
+import android.widget.TextView
 
 //첫 화면
 //가족코드 (혹은 지정된 가족 이름) 을 이용해 그룹에 참여
@@ -14,11 +18,38 @@ import android.widget.Switch
 //큐얼코드는 시간이 된다면 추가 예정
 
 class FirstActivity : AppCompatActivity() {
+
+    lateinit var nameAddTextView : TextView //닉네임을 입력해주세요!
+    lateinit var nameEditText : EditText //닉네임 입력칸
+    lateinit var checkButton : Button //확인 버튼
+    lateinit var makenewTextView : TextView //코드 번호가 없다면 새로운 그룹을 만드세요!
+    lateinit var insertButton: Button //코드 입력 버튼
+    lateinit var makeButton:Button //가족 생성 버튼
+    lateinit var groupAddTextView: TextView //그룹 코드를 입력해주세요!
+    lateinit var groupEditText : EditText //그룹 코드 입력칸
+    lateinit var checkButton2 : Button //확인 버튼
+    lateinit var welcomeTextView : TextView //환영 문구
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+        checkButton = findViewById(R.id.checkButton)
+
+        checkButton.setOnClickListener {
+            nameAddTextView.visibility=View.INVISIBLE
+            nameEditText.visibility=View.INVISIBLE
+            makenewTextView.visibility=View.VISIBLE
+            insertButton.visibility=View.VISIBLE
+            makeButton.visibility=View.VISIBLE
+            groupAddTextView.visibility=View.INVISIBLE
+            groupEditText.visibility=View.INVISIBLE
+            checkButton2.visibility=View.INVISIBLE
+            welcomeTextView.visibility=View.INVISIBLE
+        }
 
         appBarMode(false) //앱바 감추기 (전체화면)
+
+
     }
 
     //앱바 감추기 함수 (전체화면)
@@ -28,17 +59,5 @@ class FirstActivity : AppCompatActivity() {
         }else {
             supportActionBar?.hide()
         }
-    }
-}
-
-class DBManeger(context: Context, name: String, factory: SQLiteDatabase.CursorFactory?, version: Int):
-        SQLiteOpenHelper(context, name, factory, version) {
-    override fun onCreate(p0: SQLiteDatabase?) {
-        TODO("Not yet implemented")
-
-    }
-
-    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
     }
 }
